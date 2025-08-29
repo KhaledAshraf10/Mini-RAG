8f305277c790901fdd2ded7d7416669521eaa7a9
Khaled Ashraf <khaledashrafn0@gmail.com>
2025-08-29 15:19:05 +0300

    make scheme and divide the files into chunks depend on size and overlap End vid_08

--- src/routes/data.py
+++ src/routes/data.py
@@ -2,26 +2,29 @@ from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 import aiofiles
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
-    tags = ["api_v1", "data"]
+    tags=["api_v1", "data"],
 )
 
 @data_router.post("/upload/{project_id}")
-
-async def upload_data(project_id: str, file: UploadFile
-                      ,app_settings: Settings =Depends(get_settings)):
-    data_controller = DataController()
-    #validate the file properties 
-    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+async def upload_data(project_id: str, file: UploadFile,
+                      app_settings: Settings = Depends(get_settings)):
+        
     
+    # validate the file properties
+    data_controller = DataController()
+
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+
     if not is_valid:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -29,19 +32,21 @@ async def upload_data(project_id: str, file: UploadFile
                 "signal": result_signal
             }
         )
-    project_dir_path = ProjectController().get_project_path(project_id=project_id)
 
-    file_path = data_controller.generatre_uique_filename(
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path, file_id = data_controller.generate_unique_filepath(
         orig_file_name=file.filename,
         project_id=project_id
     )
-    try : 
+
+    try:
         async with aiofiles.open(file_path, "wb") as f:
             while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
                 await f.write(chunk)
     except Exception as e:
 
-        logger.error(f"Error while uploading file : {e}")
+        logger.error(f"Error while uploading file: {e}")
+
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={
@@ -51,6 +56,35 @@ async def upload_data(project_id: str, file: UploadFile
 
     return JSONResponse(
             content={
-                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "file_id": file_id
             }
         )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+
+    return file_chunks
